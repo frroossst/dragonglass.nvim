f91c2fd72b92759bf1f074ce495b47fcb76d9915
Adhyan H <adhyanpatel@gmail.com>
2022-12-20 20:06:19 -0600

    calling glow.nvim rendered with lua funcs

--- lua/dg/init.lua
+++ lua/dg/init.lua
@@ -50,4 +50,6 @@ M.win = function()
     -- vim.api.nvim_open_win(buf, false, {relative='win', row=3, col=3, width=12, height=3})
 end
 
+        
+
 return M
--- plugin/example.lua
+++ plugin/example.lua
@@ -1,3 +1,5 @@
 print("hello from plugin")
 
 vim.keymap.set("n", "<F12>", ":echo 'could not resolve link'<CR>")
+
+require("dg").win()
