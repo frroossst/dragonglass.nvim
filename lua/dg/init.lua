61cc2da43f68c37118cd275f1fabe81e1741a146
Adhyan H <adhyanpatel@gmail.com>
2022-12-23 16:47:26 -0600

    changed up code a bit

--- lua/dg/init.lua
+++ lua/dg/init.lua
@@ -1,36 +1,9 @@
+ local win, buf
 local M = {}
+
 dg_vault_path = ""
-def_opts = {
-  args = "",
-  bang = false,
-  count = -1,
-  fargs = {},
-  line1 = 1,
-  line2 = 1,
-  mods = "",
-  range = 0,
-  reg = "",
-  smods = {
-    browse = false,
-    confirm = false,
-    emsg_silent = false,
-    hide = false,
-    horizontal = false,
-    keepalt = false,
-    keepjumps = false,
-    keepmarks = false,
-    keeppatterns = false,
-    lockmarks = false,
-    noautocmd = false,
-    noswapfile = false,
-    sandbox = false,
-    silent = false,
-    split = "",
-    tab = -1,
-    unsilent = false,
-    verbose = -1,
-    vertical = false
-  }
+dg_opts = {
+    fext = ".md"
 }
 
 
@@ -38,16 +11,48 @@ M.hello = function()
     print("hello from dragonglass")
 end
 
-M.setup = function(opts)
+M.start = function()
+    local curr_buf_ext = string.sub(vim.api.nvim_buf_get_name(0), -3)
+    if curr_buf_ext == dg_opts.dg_opts then
+        M.win()
+    else
+        print("not a markdown file")
+    end
+end
+
+local function set_path(opts)
     print("Options: ", opts)
     dg_vault_path = opts.path
     print("path set to: ", dg_vault_path)
 end
 
-M.win = function()
-    require("glow").execute(def_opts)
-    -- local buf = vim.api.nvim_create_buf(false, true)
-    -- vim.api.nvim_open_win(buf, false, {relative='win', row=3, col=3, width=12, height=3})
+local function open_window()
+    local win_width = vim.o.columns
+    local win_height = vim.o.lines
+
+    local win_opts = {
+        style = "minimal",
+        relative = "editor",
+        width = win_width,
+        height = win_height,
+        --col = win_width,
+        --row = win_height,
+    }
+
+    buf = vim.api.nvim_create_buf(false, true)
+    win = vim.api.nvim_open_win(buf, true, win_opts)
+
+    vim.api.nvim_win_set_option(win, "winblend", 0)
+    vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+    vim.api.nvim_buf_set_option(buf, "filetype", "glowpreview")
+
+    local keymap_opts = { noremap = true, silent = true, buffer = buf }
+    vim.keymap.set("n", "q", close_window, keymap_opts)
+
+end
+
+local function close_window()
+    vim.api.nvim_win_close(win, true)
 end
 
 
